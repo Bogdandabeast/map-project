8bf07a30a1ba27cb6d29c458ed787a4f8e5a71a0
Bogdandabeast <bogdan.console@gmail.com>
2026-04-18 15:16:39 +0200

    feat(stoker): add XML escaping for emoji favicon SVG

--- packages/stoker/src/middlewares/serve-emoji-favicon.ts
+++ packages/stoker/src/middlewares/serve-emoji-favicon.ts
@@ -1,10 +1,24 @@
 import type { MiddlewareHandler } from 'hono'
 
+function escapeXml(unsafe: string): string {
+  return unsafe.replace(/[<>&"']/g, (c) => {
+    switch (c) {
+      case '<': return '&lt;'
+      case '>': return '&gt;'
+      case '&': return '&amp;'
+      case '"': return '&quot;'
+      case '\'': return '&apos;'
+      default: return c
+    }
+  })
+}
+
 function serveEmojiFavicon(emoji: string): MiddlewareHandler {
+  const escapedEmoji = escapeXml(emoji)
   return async (c, next) => {
     if (c.req.path === '/favicon.ico') {
       c.res.headers.set('content-type', 'image/svg+xml')
-      return c.body(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" x="-0.1em" font-size="90">${emoji}</text></svg>`)
+      return c.body(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" x="-0.1em" font-size="90">${escapedEmoji}</text></svg>`)
     }
     return next()
   }
